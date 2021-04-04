496a1cbb36da8afb2d4389e012762553404c9253
M-max125 <c.marcu60@yahoo.com>
2021-04-05 01:08:44 +0300

    Update Database.class.php

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -10,11 +10,18 @@ class Database{
     public $conn;
     
     public function __construct(){
+        //dev mode
+        // $this->host = 'localhost';
+        // $this->dbname = 'db_food_app';
+        // $this->dbuser = 'root';
+        // $this->dbpass = '';
+
+        //prod mode
+        $this->host = 'eu-cdbr-west-01.cleardb.com';
+        $this->dbname = 'heroku_4576f989dde6a1e';
+        $this->dbuser = 'bc5109bae534fb';
+        $this->dbpass = '4efea717';
         
-        $this->host = 'localhost';
-        $this->dbname = 'db_food_app';
-        $this->dbuser = 'root';
-        $this->dbpass = '';
         $dsn = "mysql:host=".$this->host.";dbname=".$this->dbname;
         
         try{
